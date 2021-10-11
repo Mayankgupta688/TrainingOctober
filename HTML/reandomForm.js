85da4e96539e5df342d4f56ab704689f34670f93
MayankGupta <mayank.gupta.6.88@gmail.com>
2021-10-11 13:30:23 +0530

    Session Three

---
+++ HTML/reandomForm.js
@@ -0,0 +1,8 @@
+
+function performActionOnClick() {
+    alert("Hello: " + document.getElementById("user_name").value)
+}
+
+function updateColor(inputColor) {
+    document.getElementById("enter_name").style.color = inputColor;
+}
