2f1c9294a5b5cba9009d55fc0fb46239392580cd
MayankGupta <mayank.gupta.6.88@gmail.com>
2021-10-19 13:45:57 +0530

    Session Six

--- employee-management/src/components/CurrentTime.js
+++ employee-management/src/components/CurrentTime.js
@@ -1,10 +1,14 @@
+
+
 export function CurrentTime(props) {
-    debugger;
+    props.userDetails.address.street = "dafasuhdfsahdasihdgiasgu";
+    props.salaryArray.push(100);
     return (
         <div>
-            <h1>Welcome {props.userDetails.salary}</h1>
+            <h1>Welcome {props.userDetails.userName}</h1>
             <h2>User Age is: {props.userDetails.userAge}</h2>
             <h3>Current Time: {props.userDetails.time}</h3>
+            <h4>Array Length {props.userDetails.address.street}</h4>
         </div>
     )
 }
--- employee-management/src/components/PassingProps.js
+++ employee-management/src/components/PassingProps.js
@@ -16,3 +16,35 @@ export function PassingProps(props) {
 // new Date().getHours()
 // new Date().getMinutes()
 // new Date().getSeconds()
+
+
+export var employeeData =    [{
+      "id": "2",
+      "createdAt": "2021-03-10T12:58:31.300Z",
+      "name": "Delores Hyatt",
+      "avatar": "https://st.depositphotos.com/1594308/4626/i/950/depositphotos_46262829-stock-photo-happy-employee.jpg"
+    },
+    {
+      "id": "4",
+      "createdAt": "2021-03-10T20:21:39.209Z",
+      "name": "Miss Sydni Mertz",
+      "avatar": "https://st.depositphotos.com/1594308/4626/i/950/depositphotos_46262829-stock-photo-happy-employee.jpg"
+    },
+    {
+      "id": "8",
+      "createdAt": "2021-03-11T00:11:14.957Z",
+      "name": "Blanca Graham",
+      "avatar": "https://st.depositphotos.com/1594308/4626/i/950/depositphotos_46262829-stock-photo-happy-employee.jpg"
+    },
+    {
+      "id": "11",
+      "createdAt": "2021-03-11T07:05:20.249Z",
+      "name": "Emile Lueilwitz",
+      "avatar": "https://st.depositphotos.com/1594308/4626/i/950/depositphotos_46262829-stock-photo-happy-employee.jpg"
+    },
+    {
+      "id": "13",
+      "createdAt": "2021-03-10T22:05:48.241Z",
+      "name": "Jayda West",
+      "avatar": "https://st.depositphotos.com/1594308/4626/i/950/depositphotos_46262829-stock-photo-happy-employee.jpghttps://st.depositphotos.com/1594308/4626/i/950/depositphotos_46262829-stock-photo-happy-employee.jpg"
+    }]
